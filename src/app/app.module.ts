cf3891ad1a729015d188f6fd3bc7c90cfb26d20d
Rodrigo Medeiros <rodrigovinicius.bsb@gmail.com>
2024-05-14 19:48:43 -0300

    feat: list book with NGRX

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { BookReducer } from './books/book.reducer';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { AppState } from './app.state';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot<AppState>({book: BookReducer})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
